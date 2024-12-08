763e11350231ae4c7b7f7a8566252490bbbbb6d2
pavlenski <galanticpavle@gmail.com>
2024-12-08 20:35:26 +0100

    remove dummy controller

--- lib/stocker_api_web/controllers/dummy_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule StockerApiWeb.DummyController do
-  use StockerApiWeb, :controller
-
-  def dummy_response(conn, params) do
-    json(conn, %{message: "nogges", params: params})
-  end
-end
