ad9a9cefb5559689adffd15f528278ca95fdddf7
pavlenski <galanticpavle@gmail.com>
2024-12-08 16:25:29 +0100

    add csv dep & update runfile

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule StockerApi.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:csv, "~> 3.0"}
     ]
   end
 
