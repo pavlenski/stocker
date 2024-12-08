84bcb1759d727e1d3341683858e9e967046659d6
pavlenski <galanticpavle@gmail.com>
2024-12-08 23:57:44 +0100

    fix mix format

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule StockerApi.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:csv, "~> 3.0"},
+      {:csv, "~> 3.0"}
     ]
   end
 
