789c53b66e2fff7c508d55a4745b0b9117861862
pavlenski <galanticpavle@gmail.com>
2024-12-09 11:35:17 +0100

    add stock prices migration

---
+++ priv/repo/migrations/20241209101745_create_stock_prices.exs
@@ -0,0 +1,18 @@
+defmodule StockerApi.Repo.Migrations.CreateStockPrices do
+  use Ecto.Migration
+
+  def change do
+    create table(:stock_prices, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :close, :decimal, null: false
+      add :date, :date, null: false
+
+      add :stock_id,
+          references(
+            :stocks,
+            type: :binary_id,
+            on_delete: :delete_all
+          )
+    end
+  end
+end
