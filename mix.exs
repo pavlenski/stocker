1fc9d82de1399c56a53f6846cd91e3bd68b437c8
pavlenski <galanticpavle@gmail.com>
2024-12-09 17:47:14 +0100

    add lib for unziping compressed files

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule StockerApi.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:csv, "~> 3.0"}
+      {:csv, "~> 3.0"},
+      {:unzip, "~> 0.12.0"}
     ]
   end
 
