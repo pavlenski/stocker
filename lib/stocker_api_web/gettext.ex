6ee22642adb3c076e00d3a0bdd45c2975cb4fd8d
pavlenski <galanticpavle@gmail.com>
2024-12-10 17:54:22 +0100

    disable verbose logging & fix warning

--- lib/stocker_api_web/gettext.ex
+++ lib/stocker_api_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule StockerApiWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :stocker_api
+  use Gettext.Backend, otp_app: :stocker_api
 end
