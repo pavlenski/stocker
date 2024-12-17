bce5751e69d966faea586f03458cae0ba1514402
pavlenski <galanticpavle@gmail.com>
2024-12-17 13:21:42 +0100

    add test job in CI

--- mix.exs
+++ mix.exs
@@ -61,6 +61,7 @@ defmodule StockerApi.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
+      citest: ["ecto.create", "ecto.migrate", "test"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
