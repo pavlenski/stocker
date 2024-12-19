55dff764dd0ed7301d645195431b140afe532a7f
pavlenski <galanticpavle@gmail.com>
2024-12-19 10:41:54 +0100

    add integrational test for trade options back

--- test/stocker_api_web/controllers/trade_options_controller_test.exs
+++ test/stocker_api_web/controllers/trade_options_controller_test.exs
@@ -11,48 +11,48 @@ defmodule StockerApiWeb.TradeOptionsControllerTest do
 
   describe "creating & testing stocks" do
     test "create stocks", _params do
-      s1 = insert(:stock)
-      s2 = insert(:stock)
+      s1 = insert(:stock, ticker: "GGG1")
+      s2 = insert(:stock, ticker: "GGG2")
 
       stocks = Repo.all(Stock)
 
       assert length(stocks) == 2
       assert %{ticker: t1} = Repo.get(Stock, s1.id)
       assert %{ticker: t2} = Repo.get(Stock, s2.id)
-      assert t1 == "TCK0"
-      assert t2 == "TCK1"
+      assert t1 == "GGG1"
+      assert t2 == "GGG2"
     end
   end
 
-  # describe "GET /stocks/:ticker/trade-options" do
-  #   test "return stock trading options", %{conn: conn} do
-  #     stock = insert(:stock)
-  #     _price1 = insert(:stock_price, stock: stock, close: Decimal.new(1), date: ~D[2020-10-10])
-  #     _price2 = insert(:stock_price, stock: stock, close: Decimal.new(7), date: ~D[2020-10-11])
-  #     _price3 = insert(:stock_price, stock: stock, close: Decimal.new(3), date: ~D[2020-10-13])
-  #     _price4 = insert(:stock_price, stock: stock, close: Decimal.new(2), date: ~D[2020-10-16])
-  #     _price4 = insert(:stock_price, stock: stock, close: Decimal.new(5), date: ~D[2020-10-17])
-  #     _price4 = insert(:stock_price, stock: stock, close: Decimal.new(4), date: ~D[2020-10-18])
+  describe "GET /stocks/:ticker/trade-options" do
+    test "return stock trading options", %{conn: conn} do
+      stock = insert(:stock)
+      _price1 = insert(:stock_price, stock: stock, close: Decimal.new(1), date: ~D[2020-10-10])
+      _price2 = insert(:stock_price, stock: stock, close: Decimal.new(7), date: ~D[2020-10-11])
+      _price3 = insert(:stock_price, stock: stock, close: Decimal.new(3), date: ~D[2020-10-13])
+      _price4 = insert(:stock_price, stock: stock, close: Decimal.new(2), date: ~D[2020-10-16])
+      _price4 = insert(:stock_price, stock: stock, close: Decimal.new(5), date: ~D[2020-10-17])
+      _price4 = insert(:stock_price, stock: stock, close: Decimal.new(4), date: ~D[2020-10-18])
 
-  #     conn =
-  #       get(
-  #         conn,
-  #         "/api/stocks/#{stock.ticker}/trade-options?date_from=2020-10-10&date_to=2020-10-18"
-  #       )
+      conn =
+        get(
+          conn,
+          "/api/stocks/#{stock.ticker}/trade-options?date_from=2020-10-10&date_to=2020-10-18"
+        )
 
-  #     json_response = json_response(conn, 200)
+      json_response = json_response(conn, 200)
 
-  #     assert %{"current_dates" => current_dates} = json_response
-  #     assert %{"former_dates" => former_dates} = json_response
-  #     assert %{"latter_dates" => latter_dates} = json_response
-  #     assert %{"single_trade" => single_trade, "multi_trade" => multi_trade} = current_dates
+      assert %{"current_dates" => current_dates} = json_response
+      assert %{"former_dates" => former_dates} = json_response
+      assert %{"latter_dates" => latter_dates} = json_response
+      assert %{"single_trade" => single_trade, "multi_trade" => multi_trade} = current_dates
 
-  #     assert former_dates == "No data for former range"
-  #     assert latter_dates == "No data for latter range"
-  #     assert single_trade["max_profit"] == "6"
-  #     assert single_trade["buy_date"] == "2020-10-10"
-  #     assert single_trade["sell_date"] == "2020-10-11"
-  #     assert multi_trade == "11"
-  #   end
-  # end
+      assert former_dates == "No data for former range"
+      assert latter_dates == "No data for latter range"
+      assert single_trade["max_profit"] == "6"
+      assert single_trade["buy_date"] == "2020-10-10"
+      assert single_trade["sell_date"] == "2020-10-11"
+      assert multi_trade == "11"
+    end
+  end
 end
